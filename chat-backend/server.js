3efe9d9e90724c2ec46231c80c9bc17540df58fe
shiekiragu12 <sheerohkiragu@gmail.com>
2023-03-07 21:46:25 +0300

    added react project

--- chat-backend/server.js
+++ chat-backend/server.js
@@ -2,16 +2,22 @@ const express = require ('express');
 const app = express();
 
 const rooms = ['general','tech','finance','crypto'];
-const cores = require ('cors');
+const cors = require ('cors');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cors());
 
 const server =  require('http').createServer(app);
-const POST = 5001;
+const PORT = 5001;
 const io= require('socket.io')(server,{
     cors:{
-        origin: 'http://localhost:3000'
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST']
     }
+})
+
+
+server.listen(PORT, ()=> {
+    console.log('check if port is being listened to', PORT)
 })
